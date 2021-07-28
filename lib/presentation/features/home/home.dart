c246b3ccb11c15173829b9cca328aa55d063f27a
Devlonoah <hemkeys@gmail.com>
2021-07-28 16:15:14 +0100

    print() method cleanup& genera; cleanup

--- lib/presentation/features/home/home.dart
+++ lib/presentation/features/home/home.dart
@@ -113,7 +113,6 @@ class HomeBody extends StatelessWidget {
                             );
                           },
                         );
-                                 arguments: note.id),);
                       },
                     );
                   }
